6f3130d2d778ee485f3c0c101eab8cc7d440bb80
thomas0092 <thomas.severgnini@refactormi.com>
2024-08-02 16:31:17 +0200

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,23 +1,31 @@
 const fetchDitto = () => {
-    return fetch("https://pokeapi.co/api/v2/pokemon/ditto").then((response) =>
-      response.json()
-    );
-  };
-  
-  const fetchCharizard = () => {
-    return fetch("https://pokeapi.co/api/v2/pokemon/charizard").then((response) =>
-      response.json()
-    );
-  };
-  
-  const fetchPikachu = () => {
-    return fetch("https://pokeapi.co/api/v2/pokemon/pikachu").then((response) =>
-      response.json()
-    );
-  };
-  const start = performance.now();
-  fetchDitto();
-  fetchCharizard();
-  fetchPikachu();
-  const end = performance.now();
-  console.log(`Tempo di esecuzione: ${end - start} millisecondi`);
+  return fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+    .then(response => response.json());
+};
+ 
+const fetchCharizard = () => {
+  return fetch("https://pokeapi.co/api/v2/pokemon/charizard")
+    .then(response => response.json());
+};
+ 
+const fetchPikachu = () => {
+  return fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+    .then(response => response.json());
+};
+ 
+const start = performance.now();
+ 
+fetchDitto()
+  .then(ditto => {
+    console.log(ditto);
+    return fetchCharizard();
+  })
+  .then(charizard => {
+    console.log(charizard);
+    return fetchPikachu();
+  })
+  .then(pikachu => {
+    console.log(pikachu);
+    const end = performance.now();
+    console.log(`Tempo di esecuzione: ${end - start} millisecondi`);
+});
